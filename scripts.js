7af130a9c526dbc137126ac2a3f2df2d4ad8d9a8
David <DavidDong5252@gmail.com>
2021-12-09 17:13:09 -0500

    Updated navbar, beautified text, fixed descriptions
    Made nnavbar always sticky and made transitions smooth and accurate. Also fixed description errors and icons.

--- scripts.js
+++ scripts.js
@@ -1,49 +1,36 @@
-// activate stickyWhenScrolling when scrolling
-window.onscroll = function() {
-	stickyWhenScrolling()
-};
-var navlist = document.getElementById("navlist");
-var sticky = navlist.offsetTop;
-
-// makes navlist stick to the top
-function stickyWhenScrolling() {
-	if (window.scrollY >= sticky) {
-      navlist.classList.add("sticky")
-    }
-    else {
-      navlist.classList.remove("sticky");
-    }
-}
-
 // sets to first project
 var slideIndex = 1;
 showSlides(slideIndex);
 
 // changes slide when left/right button is pressed
 function iterateSlide(n) {
-  showSlides(slideIndex += n);
+    showSlides(slideIndex += n);
 }
 
 // jumps to project selected by dots
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+    showSlides(slideIndex = n);
 }
 
 // shows current slide and updates dots
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = slides.length} 
-  if (n < 1) {slideIndex = 1}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = slides.length
+    }
+    if (n < 1) {
+        slideIndex = 1
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
 }
 
 function witcherHover(n) {
